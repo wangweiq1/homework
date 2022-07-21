03d27e795805a470e7c284af3648db909c9f16ae
wangwei <169@>
2022-07-21 20:06:17 +0800

    提交了form

--- component/vue.config.js
+++ component/vue.config.js
@@ -1,7 +1,8 @@
+
 module.exports = {
   devServer: {
     open: false, // 编译完成是否打开网页
-    host: "0.0.0.0", // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
+    host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
     port: 9999, // 访问端口
     https: false, // 编译失败时刷新页面
     hot: true, // 开启热加载
@@ -11,16 +12,16 @@ module.exports = {
         target: process.env.VUE_API_DEV_TARGET, // API服务器的地址
         changeOrigin: true,
         pathRewrite: {
-          [`^${process.env.VUE_APP_API}`]: "",
-        },
-      },
-    },
+          [`^${process.env.VUE_APP_API}`]: ''
+        }
+      }
+    }
   },
   css: {
     loaderOptions: {
       scss: {
-        prependData: "@import './src/styles/scssconfig.scss';",
-      },
-    },
-  },
-};
+        prependData: "@import './src/styles/scssconfig.scss';"
+      }
+    }
+  }
+}
